687f9770f1953e0790601c8c16897a4b9720f544
thr2240 <thomasr2240@gmail.com>
2023-02-07 15:56:16 +0100

    final

--- main.go
+++ main.go
@@ -29,8 +29,6 @@ func UploadFile(s *session.Session, bucketName string, objectKey string, payload
 		}
 	}
 	
-	fmt.Println(body)
-
 	_, err := s3.New(s).PutObject(&s3.PutObjectInput{
 		Bucket: aws.String(bucketName),
 		Key:    aws.String(objectKey),
@@ -66,81 +64,84 @@ func HandleRequest() {
 	t := time.Now()
 	rounded := time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location()).UTC()
 
-	startTime := aws.Time(rounded.Add(time.Second * -3600 * 24))
-	endTime := aws.Time(rounded.Add(time.Second * -3600 * 1))
-	
-	// fmt.Println("startTime", startTime)
-	// fmt.Println("endTime", endTime)
-	
-	for _, region := range regions.Regions {
-		// Create new EC2 client
-		regionName := *region.RegionName
-		// fmt.Println(regionName)
+	for i:= 0; i < 10; i ++ {
+		startTime := aws.Time(rounded.Add(time.Second * time.Duration(-3600 * (24 * (i + 1)))))
+		endTime := aws.Time(rounded.Add(time.Second * time.Duration(-3600 * (1 + 24 * i ))))
+		
+		// fmt.Println("startTime", startTime)
+		// fmt.Println("endTime", endTime)
+		
+		for _, region := range regions.Regions {
+			// Create new EC2 client
+			regionName := *region.RegionName
+			// fmt.Println(regionName)
 
-		regionConf := aws.NewConfig().WithRegion(regionName)
-		client = ec2.New(sess, regionConf)
-		cw := cloudwatch.New(sess, regionConf)
-		stsConn := sts.New(sess, regionConf)
-		if err != nil {
-			fmt.Println("Error", err)
-		}
+			regionConf := aws.NewConfig().WithRegion(regionName)
+			client = ec2.New(sess, regionConf)
+			cw := cloudwatch.New(sess, regionConf)
+			stsConn := sts.New(sess, regionConf)
+			if err != nil {
+				fmt.Println("Error", err)
+			}
 
-		result, err := client.DescribeInstances(nil)
+			result, err := client.DescribeInstances(nil)
 
-		if err != nil {
-			fmt.Println("Error", err)
-		}
+			if err != nil {
+				fmt.Println("Error", err)
+			}
 
-		for _, r := range result.Reservations {
-			for _, i := range r.Instances {
-				search := cloudwatch.GetMetricStatisticsInput{
-					StartTime:  startTime,
-					EndTime:    endTime,
-					MetricName: aws.String("CPUUtilization"),
-					Period:     &period,
-					Statistics: []*string{aws.String("Maximum")},
-					Namespace:  aws.String("AWS/EC2"),
-					Dimensions: []*cloudwatch.Dimension{{Name: aws.String("InstanceId"), Value: i.InstanceId}},
-				}
-				// fmt.Printf("InstanceID: %v State: %v\n", *i.InstanceId, i.State.Name)
-				resp, err := cw.GetMetricStatistics(&search)
-				
-				if err != nil {
-					fmt.Println("Error", err)
-				}
+			for _, r := range result.Reservations {
+				for _, i := range r.Instances {
+					search := cloudwatch.GetMetricStatisticsInput{
+						StartTime:  startTime,
+						EndTime:    endTime,
+						MetricName: aws.String("CPUUtilization"),
+						Period:     &period,
+						Statistics: []*string{aws.String("Maximum")},
+						Namespace:  aws.String("AWS/EC2"),
+						Dimensions: []*cloudwatch.Dimension{{Name: aws.String("InstanceId"), Value: i.InstanceId}},
+					}
+					// fmt.Printf("InstanceID: %v State: %v\n", *i.InstanceId, i.State.Name)
+					resp, err := cw.GetMetricStatistics(&search)
+					
+					if err != nil {
+						fmt.Println("Error", err)
+					}
 
-				fmt.Println(resp)
+					fmt.Println(resp)
 
-				// fmt.Println(resp.Datapoints)
-				outCallerIdentity, err := stsConn.GetCallerIdentity(&sts.GetCallerIdentityInput{})
-				if err != nil {
-					fmt.Println("Error", err)
-				}
-				accountID := *outCallerIdentity.Account
-				// fmt.Println(accountID)
+					// fmt.Println(resp.Datapoints)
+					outCallerIdentity, err := stsConn.GetCallerIdentity(&sts.GetCallerIdentityInput{})
+					if err != nil {
+						fmt.Println("Error", err)
+					}
+					accountID := *outCallerIdentity.Account
+					// fmt.Println(accountID)
 
-				for _, record := range resp.Datapoints {
-					temp := strconv.FormatFloat(*record.Maximum, 'f', 6, 64)
-					intTemp, _ := strconv.ParseInt(accountID, 10, 64)
-					accountTemp := strconv.FormatInt(intTemp, 10)
-					tempTime := record.Timestamp.String()
-					payload = append(payload, []string{
-						*i.InstanceId,
-						temp,
-						accountTemp,
-						regionName,
-						tempTime,
-					})
+					for _, record := range resp.Datapoints {
+						temp := strconv.FormatFloat(*record.Maximum, 'f', 6, 64)
+						intTemp, _ := strconv.ParseInt(accountID, 10, 64)
+						accountTemp := strconv.FormatInt(intTemp, 10)
+						tempTime := record.Timestamp.String()
+						payload = append(payload, []string{
+							*i.InstanceId,
+							temp,
+							accountTemp,
+							regionName,
+							tempTime,
+						})
+					}
 				}
 			}
 		}
+		uploadFileName := fmt.Sprintf("CPUUtilization%02v.%02v.%v.csv", int(startTime.Month()), startTime.Day(), startTime.Year())
+		err = UploadFile(sess, "monitoring-v0-test-new-files-per-day", uploadFileName, payload)
+		if err != nil {
+			fmt.Println("Error", err)
+		}
+		fmt.Println("Upload Finished")
 	}
-	uploadFileName := fmt.Sprintf("CPUUtilization%02v.%02v.%v.csv", int(rounded.Month()), rounded.Day(), rounded.Year())
-	err = UploadFile(sess, "monitoring-v0-test-new-files-per-day", uploadFileName, payload)
-	if err != nil {
-		fmt.Println("Error", err)
-	}
-	fmt.Println("Upload Finished")
+	
 }
 
 func main() {
